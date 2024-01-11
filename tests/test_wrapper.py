01606b05d55d607ffd690fc981f7fd30a12d11cc
apirrone <awanegenebayo@gmail.com>
2024-01-11 11:10:49 +0100

    adding get_connected_devices() in utils.py

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -6,3 +6,10 @@ from src.depthai_wrappers.wrapper import Wrapper
 def test_wrapper():
     with pytest.raises(TypeError):
         w = Wrapper("dummy", 50, resize=(1280, 720), rectify=True)
+
+
+def test_get_connected_devices():
+    from src.depthai_wrappers.utils import get_connected_devices
+
+    devices = get_connected_devices()
+    assert devices == {}
