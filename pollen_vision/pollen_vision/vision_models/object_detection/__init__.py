b0d84e4cfa5a915be47beda837818dee8b39a6c3
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2024-02-23 15:33:26 +0100

    Make import of ObjectDescriptionGenerator easier.

--- pollen_vision/pollen_vision/vision_models/object_detection/__init__.py
+++ pollen_vision/pollen_vision/vision_models/object_detection/__init__.py
@@ -1,2 +1,3 @@
 from .owl_vit.owl_vit_wrapper import OwlVitWrapper  # noqa: F401
 from .recognize_anything.RAM_wrapper import RAM_wrapper  # noqa: F401
+from .recognize_anything.objects_descriptions_generator import ObjectDescriptionGenerator  # noqa: F401
