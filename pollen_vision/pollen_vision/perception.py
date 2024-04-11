d1002169241de2597472d9fbf7c49b73952b2706
apirrone <awanegenebayo@gmail.com>
2024-04-11 16:20:48 +0200

    fixing errors

--- pollen_vision/pollen_vision/perception.py
+++ pollen_vision/pollen_vision/perception.py
@@ -54,25 +54,33 @@ class Perception:
             if elapsed < 1 / self.freq:
                 time.sleep(1 / self.freq - elapsed)
 
-            self.OF.tick()
-
-            if len(self.tracked_objects) == 0:
-                print("No objects to track. Use set_tracked_objects().")
-                self._lastTick = time.time()  # Lame
-                return
-
             data, _, _ = self.cam.get_data()
             self.last_im = data["left"]
             self.last_depth = data["depth"]
 
+            self.OF.tick()
+            if self.visualize and self.last_im is not None:
+                objs = self.get_objects()
+                annotated = self.last_im.copy()
+                for obj in objs:
+                    pos2D = (int((obj["bbox"][0] + obj["bbox"][2]) / 2), int((obj["bbox"][1] + obj["bbox"][3]) / 2))
+                    annotated = cv2.circle(self.last_im, pos2D, 5, (0, 255, 0), -1)
+                cv2.imshow("annotated", annotated)
+                cv2.waitKey(1)
+
+            if len(self.tracked_objects) == 0:
+                print("No objects to track. Use set_tracked_objects().")
+                self._lastTick = time.time()  # Lame
+                continue
+
             self.last_predictions = self.OWL.infer(
-                cv2.cvtColor(self.last_im, cv2.COLOR_BGR2RGB), self.tracked_objects, detection_threshold=0.1
+                cv2.cvtColor(self.last_im, cv2.COLOR_BGR2RGB), self.tracked_objects, detection_threshold=0.2
             )
 
             if len(self.last_predictions) == 0:
                 self._lastTick = time.time()  # Lame
                 print("No objects detected.")
-                return
+                continue
 
             bboxes = get_bboxes(self.last_predictions)
             labels = get_labels(self.last_predictions)
@@ -82,16 +90,6 @@ class Perception:
                 T_world_object = get_object_pose_in_world(self.last_depth, mask, self.T_world_cam, self.cam.get_K())
                 pos = T_world_object[:3, 3]
                 self.OF.push_observation(labels[i], pos, bboxes[i])
-
-            if self.visualize:
-                annotated = self.A.annotate(self.last_im, self.last_predictions, self.last_masks)
-                objs = self.get_objects()
-                for obj in objs:
-                    # pos2D is the center of bbox
-                    pos2D = (int((obj["bbox"][0] + obj["bbox"][2]) / 2), int((obj["bbox"][1] + obj["bbox"][3]) / 2))
-                    annotated = cv2.circle(annotated, pos2D, 5, (0, 255, 0), -1)
-                cv2.imshow("annotated", annotated)
-                cv2.waitKey(1)
 
             self._lastTick = time.time()
 
@@ -109,7 +107,7 @@ if __name__ == "__main__":
     S = SDKWrapper(get_config_file_path("CONFIG_SR"), compute_depth=True)
     T_world_cam = fv_utils.make_pose([0.03, -0.15, 0.1], [0, 0, 0])
     perception = Perception(S, T_world_cam, freq=10)
-    perception.set_tracked_objects(["mug"])
+    perception.set_tracked_objects(["mug", "green tape roll"])
     perception.start(visualize=True)
 
     while True:
