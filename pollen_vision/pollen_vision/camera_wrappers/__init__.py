d5deeeb9c641b0b830bedba59c955477e4b743ec
apirrone <awanegenebayo@gmail.com>
2024-04-30 15:44:10 +0200

    adding realsense wrapper to camera wrappers

--- pollen_vision/pollen_vision/camera_wrappers/__init__.py
+++ pollen_vision/pollen_vision/camera_wrappers/__init__.py
@@ -1,3 +1,4 @@
 from .camera_wrapper import CameraWrapper  # noqa: F401
 from .depthai.sdk import SDKWrapper  # noqa: F401
 from .depthai.teleop import TeleopWrapper  # noqa: F401
+from .realsense.realsense_wrapper import RealsenseWrapper  # noqa: F401
---
+++ pollen_vision/pollen_vision/camera_wrappers/realsense/realsense_wrapper.py
@@ -0,0 +1,77 @@
+from datetime import timedelta
+from typing import Dict, Tuple
+
+import cv2
+import numpy as np
+import numpy.typing as npt
+import pyrealsense2 as rs
+from numpy._typing import NDArray
+from pollen_vision.camera_wrappers import CameraWrapper
+
+
+class RealsenseWrapper(CameraWrapper):  # type: ignore
+    def __init__(self) -> None:
+        self.pipeline = rs.pipeline()
+        config = rs.config()
+
+        pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+        pipeline_profile = config.resolve(pipeline_wrapper)
+        # device = pipeline_profile.get_device().first_depth_sensor()
+        # device.set_option(rs.option.depth_units, 0.01)
+
+        config.enable_stream(rs.stream.depth, rs.format.z16, 30)
+        other_stream, other_format = rs.stream.color, rs.format.rgb8
+        config.enable_stream(other_stream, other_format, 30)
+
+        self.pipeline.start(config)
+        profile = self.pipeline.get_active_profile()
+
+        depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
+        intrinsics = depth_profile.get_intrinsics()
+        w, h = intrinsics.width, intrinsics.height
+        fx = float(intrinsics.fx)  # Focal length of x
+        fy = float(intrinsics.fy)  # Focal length of y
+        ppx = float(intrinsics.ppx)  # Principle Point Offsey of x (aka. cx)
+        ppy = float(intrinsics.ppy)  # Principle Point Offsey of y (aka. cy)
+        axs = 0.0  # Axis skew
+
+        self.K = np.array([[fx, axs, ppx], [0.0, fy, ppy], [0.0, 0.0, 1.0]])
+
+        self.pc = rs.pointcloud()
+
+    def get_data(self) -> Tuple[Dict[str, npt.NDArray[np.uint8]], Dict[str, float], Dict[str, timedelta]]:
+        success, frames = self.pipeline.try_wait_for_frames(timeout_ms=10)
+
+        data = {}
+        latency = {}
+        ts = {}
+        if not success:
+            return None, None, None
+
+        depth = np.asanyarray(frames.get_depth_frame().get_data())
+        color_frame = np.asanyarray(frames.get_color_frame().get_data())
+
+        data["left"] = cv2.cvtColor(color_frame, cv2.COLOR_BGR2RGB)
+        depth = depth * 0.01
+        depth = np.array(depth, dtype=np.float32)
+        data["depth"] = depth
+
+        return data, latency, ts
+
+    def get_K(self, left: bool = True) -> npt.NDArray[np.float32]:
+        return self.K
+        # return np.array([[615.0, 0.0, 320.0], [0.0, 615.0, 240.0], [0.0, 0.0, 1.0]])
+
+
+if __name__ == "__main__":
+    wrapper = RealsenseWrapper()
+    while True:
+        data, _, _ = wrapper.get_data()
+        if data is None:
+            continue
+        left = data["left"]
+        depth = data["depth"]
+        # print(depth[depth != 0.0])
+        cv2.imshow("left", left)
+        cv2.imshow("depth", depth)
+        cv2.waitKey(1)
