a9d5c04026e3bc3df2a159b934c9ac8b3604c2db
apirrone <awanegenebayo@gmail.com>
2024-05-17 13:53:38 +0200

    fix depthanything wrapper expecting pil image

--- pollen_vision/pollen_vision/vision_models/monocular_depth_estimation/depth_anything_wrapper.py
+++ pollen_vision/pollen_vision/vision_models/monocular_depth_estimation/depth_anything_wrapper.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.typing as npt
+from PIL import Image
 from transformers import pipeline
 
 
@@ -10,6 +11,6 @@ class DepthAnythingWrapper:
         self.pipe = pipeline(task="depth-estimation", model=self.checkpoint)
 
     def get_depth(self, rgb: npt.NDArray[np.uint8]) -> npt.NDArray[np.float32]:
-        depth = np.asarray(self.pipe(rgb)["depth"])
+        depth = np.asarray(self.pipe(Image.fromarray(rgb))["depth"])  # type: ignore [no-untyped-call]
         depth = np.max(depth.flatten()) - depth
         return depth
